646036e08eb2c0fa4df5dfd734b411ecfd31ca2c
Martin Schmid <m.schmid@equitania.com>
2016-09-01 21:25:37 +0200

    [CHG] Übersetzung im Kalkulationsverfahren

--- addons/stock_account/__openerp__.py
+++ addons/stock_account/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'WMS Accounting',
-    'version': '1.1.2',
+    'version': '1.1.3',
     'author': 'OpenERP SA',
     'summary': 'Inventory, Logistic, Valuation, Accounting',
     'description': """
