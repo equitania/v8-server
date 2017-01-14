0db6166eaf59043e69c9fd9f18303afaaaaed12f
Martin Schmid <m.schmid@equitania.com>
2017-01-14 18:58:07 +0100

    [CHG] eq_myodoo_backend_theme auf “auto_install” gesetzt.

--- addons/eq_myodoo_backend_theme/__openerp__.py
+++ addons/eq_myodoo_backend_theme/__openerp__.py
@@ -3,7 +3,7 @@
     'name' : 'MyOdoo Backend Theme v8',
     'license': 'AGPL-3',
     'category' : 'Theme/Backend',
-    'version' : '1.1.11',
+    'version' : '1.1.12',
     'summary': 'Backend, Theme, Equitania, MyOdoo',
     'description': """
     MyOdoo Backend Theme v8
@@ -36,7 +36,7 @@
 
     # Technical
     'installable': True,
-    'auto_install': False,
-    'application': False,
+    'auto_install': True,
+    'application': True,
 
 }
