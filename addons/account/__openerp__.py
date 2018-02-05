90aa277462b270e2234a5390f8716e1f913a7b0e
Martin Schmid <m.schmid@equitania.com>
2018-02-05 12:32:03 +0100

    [CHG] Abgleich mit Odoo 8.0 - Stand 05.02.2018

--- addons/account/__openerp__.py
+++ addons/account/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name' : 'eInvoicing',
-    'version' : '1.1.170908',
+    'version' : '1.1.180205',
     'author' : 'OpenERP SA',
     'category' : 'Accounting & Finance',
     'description' : """
--- addons/point_of_sale/__openerp__.py
+++ addons/point_of_sale/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Point of Sale',
-    'version': '1.0.170715',
+    'version': '1.0.180205',
     'category': 'Point Of Sale',
     'sequence': 6,
     'summary': 'Touchscreen Interface for Shops',
