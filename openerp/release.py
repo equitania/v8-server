fa5505560b4da13627530a7d14a064d5a7c4dad8
Martin Schmid <m.schmid@equitania.com>
2018-07-15 21:01:36 +0200

    [CHG] Versionsnummer für neuen Release Manager angepasst.

--- openerp/release.py
+++ openerp/release.py
@@ -30,7 +30,7 @@ RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA,
 # properly comparable using normal operarors, for example:
 #  (6,1,0,'beta',0) < (6,1,0,'candidate',1) < (6,1,0,'candidate',2)
 #  (6,1,0,'candidate',2) < (6,1,0,'final',0) < (6,1,2,'final',0)
-version_info = (8, 1, 0, FINAL, 0)
+version_info = (8, 2018, 0, FINAL, '0201')
 
 # myBuild add from MyOdoo in format YYMMCOUNT for Releasemanager
 myBuild = '180201'
